830a230a9ff08c2d0ebeaa1a10dbb141ca544998
KarolSzymaniak <inz.karol.szymaniak@gmail.com>
2022-04-02 09:08:08 +0200

    Add JPA session in App class

---
+++ src/main/java/App.java
@@ -0,0 +1,21 @@
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class App {
+
+
+    private  static EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("unit");
+    public static void main(String[] args) {
+
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+        entityManager.getTransaction().begin();
+
+
+
+        entityManager.getTransaction().commit();
+        entityManager.close();
+
+    }
+}
